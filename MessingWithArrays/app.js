3344968ca855918c799c7adc077e97fa22a74f26
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-03-29 18:06:53 -0500

    Initial commit

---
+++ MessingWithArrays/app.js
@@ -0,0 +1,17 @@
+let topsSongs = [
+  "First time ever I saw your face",
+  "God only knows",
+  "A day in the life",
+  "Life on mars"
+];
+console.log(topsSongs);
+
+topsSongs.push("Fortunate Son"); // "pushes" the item on to the end of the array
+
+console.log(topsSongs);
+
+let last = topsSongs.pop(); // removes the last item from the array and returns the removed value
+
+console.log(topsSongs);
+
+console.log(last);
