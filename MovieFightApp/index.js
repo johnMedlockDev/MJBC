626dc3be532031d4fac62a74bfbcec4746801534
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-07 08:10:57 -0500

    finished up timer and first stage of move fight
    DOM manipulation, api fetch with axios, making components resusable.

---
+++ MovieFightApp/index.js
@@ -0,0 +1,80 @@
+document.title = "Movie Fight App";
+
+// make request to api with axios lib
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      // find out how to sumbit request to api from docs
+      apikey: key, // replace
+      //   pass user search request from some kind of input
+      s: searchTerm,
+    },
+  });
+
+  if (response.data.Error) {
+    return [];
+  }
+
+  return response.data.Search;
+};
+
+createAutoComplete({
+  root: document.querySelector(".autocomplete"),
+});
+createAutoComplete({
+  root: document.querySelector(".autocomplete-2"),
+});
+createAutoComplete({
+  root: document.querySelector(".autocomplete-3"),
+});
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      // find out how to sumbit request to api from docs
+      apikey: "eb462547",
+      //   pass user search request from some kind of input
+      i: movie.imdbID,
+    },
+  });
+  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+};
+
+const movieTemplate = (movieDetail) => {
+  return `
+<article class="media">
+    <figure class="media-left">
+        <p class="image">
+            <img src="${movieDetail.Poster}" />
+        </p>
+    </figure>
+    <div class="media-content">
+        <div class="content">
+            <h1>${movieDetail.Title}</h1>
+            <h4>${movieDetail.Genre}</h4>
+            <p>${movieDetail.Plot}</p>
+        </div>
+    </div>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+</article>
+<article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
+</article>
+    `;
+};
