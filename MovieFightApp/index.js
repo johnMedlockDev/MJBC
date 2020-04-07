bdcfd4da7bfe3fb8bba364b5c33cd35813c03c34
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-07 16:59:29 -0500

    finished movie fight app

--- MovieFightApp/index.js
+++ MovieFightApp/index.js
@@ -1,46 +1,137 @@
 document.title = "Movie Fight App";
 
-// make request to api with axios lib
-const fetchData = async (searchTerm) => {
-  const response = await axios.get("http://www.omdbapi.com/", {
-    params: {
-      // find out how to sumbit request to api from docs
-      apikey: key, // replace
-      //   pass user search request from some kind of input
-      s: searchTerm,
-    },
-  });
+// // make request to api with axios lib
+// const fetchData = async (searchTerm) => {
+//   const response = await axios.get("http://www.omdbapi.com/", {
+//     params: {
+//       // find out how to sumbit request to api from docs
+//       apikey: key, // replace
+//       //   pass user search request from some kind of input
+//       s: searchTerm,
+//     },A
+//   });
 
-  if (response.data.Error) {
-    return [];
-  }
+//   if (response.data.Error) {
+//     return [];
+//   }
 
-  return response.data.Search;
+//   return response.data.Search;
+// };
+
+const autoConpleteConfig = {
+  renderOption(movie) {
+    const imgSrc = movie.Poster === "N/A" ? "" : movie.Poster;
+    return `
+        <img src="${imgSrc}" />
+        ${movie.Title} (${movie.Year})
+        `;
+  },
+  inputValue(movie) {
+    return movie.Title;
+  },
+  async fetchData(searchTerm) {
+    const response = await axios.get("http://www.omdbapi.com/", {
+      params: {
+        // find out how to sumbit request to api from docs
+        apikey: key, // replace
+        //   pass user search request from some kind of input
+        s: searchTerm,
+      },
+    });
+
+    if (response.data.Error) {
+      return [];
+    }
+
+    return response.data.Search;
+  },
 };
 
 createAutoComplete({
-  root: document.querySelector(".autocomplete"),
+  ...autoConpleteConfig,
+  root: document.querySelector("#left-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#left-summary"), "left");
+  },
 });
 createAutoComplete({
-  root: document.querySelector(".autocomplete-2"),
-});
-createAutoComplete({
-  root: document.querySelector(".autocomplete-3"),
+  ...autoConpleteConfig,
+  root: document.querySelector("#right-autocomplete"),
+  onOptionSelect(movie) {
+    document.querySelector(".tutorial").classList.add("is-hidden");
+    onMovieSelect(movie, document.querySelector("#right-summary"), "right");
+  },
 });
 
-const onMovieSelect = async (movie) => {
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
   const response = await axios.get("http://www.omdbapi.com/", {
     params: {
       // find out how to sumbit request to api from docs
-      apikey: key,
+      apikey: key, //replace
       //   pass user search request from some kind of input
       i: movie.imdbID,
     },
   });
-  document.querySelector("#summary").innerHTML = movieTemplate(response.data);
+  summaryElement.innerHTML = movieTemplate(response.data);
+
+  if (side === "left") {
+    leftMovie = response.data;
+  } else {
+    rightMovie = response.data;
+  }
+
+  if (leftMovie && rightMovie) {
+    runComparison();
+  }
+};
+
+const runComparison = () => {
+  const leftSideStats = document.querySelectorAll(
+    "#left-summary .notification"
+  );
+  const rightSideStats = document.querySelectorAll(
+    "#right-summary .notification"
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove("is-primary");
+      leftStat.classList.add("is-warning");
+    } else {
+      rightStat.classList.remove("is-primary");
+      rightStat.classList.add("is-warning");
+    }
+  });
 };
 
 const movieTemplate = (movieDetail) => {
+  // parsed values from api request
+  const dollars = parseInt(
+    movieDetail.BoxOffice.replace(/\$/g, "").replace(/,/g, "")
+  );
+  const metascore = parseInt(movieDetail.Metascore);
+  const imdbRating = parseFloat(movieDetail.imdbRating);
+  const imdbVotes = parseInt(movieDetail.imdbVotes.replace(/,/g, ""));
+
+  //   capture awards from string loop string on " " check if nan wiht parseint return value to pre finally  return the final value.
+  const awards = movieDetail.Awards.split(" ").reduce((prev, word) => {
+    const value = parseInt(word);
+    if (isNaN(value)) {
+      return prev;
+    } else {
+      return prev + value;
+    }
+  }, 0);
+
   return `
 <article class="media">
     <figure class="media-left">
@@ -56,23 +147,24 @@ const movieTemplate = (movieDetail) => {
         </div>
     </div>
 </article>
-<article class="notification is-primary">
+
+<article data-value="${awards}" class="notification is-primary">
     <p class="title">${movieDetail.Awards}</p>
     <p class="subtitle">Awards</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${dollars}" class="notification is-primary">
     <p class="title">${movieDetail.BoxOffice}</p>
     <p class="subtitle">Box Office</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${metascore}" class="notification is-primary">
     <p class="title">${movieDetail.Metascore}</p>
     <p class="subtitle">Metascore</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${imdbRating}" class="notification is-primary">
     <p class="title">${movieDetail.imdbRating}</p>
     <p class="subtitle">IMDB Rating</p>
 </article>
-<article class="notification is-primary">
+<article data-value="${imdbVotes}" class="notification is-primary">
     <p class="title">${movieDetail.imdbVotes}</p>
     <p class="subtitle">IMDB Votes</p>
 </article>
