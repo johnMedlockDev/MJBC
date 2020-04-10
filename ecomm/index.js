7055ebdbe6be2dee3c61263c21eb5d6ef5869f05
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-10 08:18:51 -0500

    refactor index.js

--- ecomm/index.js
+++ ecomm/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
-const usersRepo = require("./repositories/users");
+const authRouter = require("./routes/admin/auth");
 
 const app = express();
 
@@ -11,76 +11,8 @@ app.use(
     keys: ["ahsjdk23fhl"],
   })
 );
-app.get("/signup", (req, res) => {
-  res.send(`
-  <div>
-  Your id is: ${req.session.userId}
-    <form method="POST">
-        <input name="email" placeholder="email"/>
-        <input name="password" placeholder="password" />
-        <input name="passwordConfirmation" placeholder="password confirmation" />
-        <button>Sign Up</button>
-    </form>
-  </div>
-  `);
-});
-
-app.post("/signup", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-
-  //   Create a user in a repo
-  const user = await usersRepo.create({ email, password });
-  // req.session is added by cookie session
-  // Store id of user in cookie
-  req.session.userId = user.id;
-  res.send("Account created!!!");
-});
-
-app.get("/signout", (req, res) => {
-  req.session = null;
-  res.send("You are signed out");
-});
-
-app.get("/signin", (req, res) => {
-  res.send(`
-<div>
-  <form method="POST">
-      <input name="email" placeholder="email"/>
-      <input name="password" placeholder="password" />
-      <button>Sign In</button>
-  </form>
-</div>
-`);
-});
+app.use(authRouter);
 
 app.listen(3000, () => {
   console.log("Listening ");
 });
-
-// homebrew bodyParser
-// const bodyParser = (req, res, next) => {
-//     if (req.method === "POST") {
-//       req.on("data", (data) => {
-//         const parsed = data.toString("utf8").split("&");
-//         const formData = {};
-//         for (let pair of parsed) {
-//           const [key, value] = pair.split("=");
-//           formData[key] = value;
-//         }
-//         req.body = formData;
-//         next();
-//       });
-//     } else {
-//       next();
-//     }
-//   };
--- ecomm/repositories/users.js
+++ ecomm/repositories/users.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 // node module for cryptography
 const crypto = require("crypto");
 
+const util = require("util");
+const scrypt = util.promisify(crypto.scrypt);
+
 class UsersRepository {
   // creates repositary instance
   constructor(filename) {
@@ -33,11 +36,26 @@ class UsersRepository {
   //   Creates a new record in the file
   async create(attrs) {
     attrs.id = this.randomId();
+
+    const salt = crypto.randomBytes(8).toString("hex");
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buf.toString("hex")}.${salt}`,
+    };
+    records.push(record);
 
     await this.writeAll(records);
-    return attrs;
+    return record;
+  }
+
+  async comparePasswords(saved, supplied) {
+    const [hashed, salt] = saved.split(".");
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hasheSuppliedBuf.toString("hex");
   }
 
   //  writes all users to file
