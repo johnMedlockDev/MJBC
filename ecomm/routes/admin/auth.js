17a664856a5eef507637f7189579d810a1dfda3d
John Medlock <42301475+johnMedlockDev@users.noreply.github.com>
2020-04-11 08:01:46 -0500

    done

--- ecomm/routes/admin/auth.js
+++ ecomm/routes/admin/auth.js
@@ -1,47 +1,48 @@
-const express = require("express");
-const { handleErrors } = require("./middlewares");
-const usersRepo = require("../../repositories/users");
-const signupTemplate = require("../../views/admin/auth/signup");
-const signinTemplate = require("../../views/admin/auth/signin");
+const express = require('express');
+
+const { handleErrors } = require('./middlewares');
+const usersRepo = require('../../repositories/users');
+const signupTemplate = require('../../views/admin/auth/signup');
+const signinTemplate = require('../../views/admin/auth/signin');
 const {
   requireEmail,
   requirePassword,
   requirePasswordConfirmation,
   requireEmailExists,
-  requireValidPasswordForUser,
-} = require("./validators");
+  requireValidPasswordForUser
+} = require('./validators');
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
+router.get('/signup', (req, res) => {
   res.send(signupTemplate({ req }));
 });
 
 router.post(
-  "/signup",
+  '/signup',
   [requireEmail, requirePassword, requirePasswordConfirmation],
-  handleErrors(signinTemplate),
+  handleErrors(signupTemplate),
   async (req, res) => {
     const { email, password } = req.body;
     const user = await usersRepo.create({ email, password });
 
     req.session.userId = user.id;
 
-    res.send("Account created!!!");
+    res.redirect('/admin/products');
   }
 );
 
-router.get("/signout", (req, res) => {
+router.get('/signout', (req, res) => {
   req.session = null;
-  res.send("You are logged out");
+  res.send('You are logged out');
 });
 
-router.get("/signin", (req, res) => {
+router.get('/signin', (req, res) => {
   res.send(signinTemplate({}));
 });
 
 router.post(
-  "/signin",
+  '/signin',
   [requireEmailExists, requireValidPasswordForUser],
   handleErrors(signinTemplate),
   async (req, res) => {
@@ -51,7 +52,7 @@ router.post(
 
     req.session.userId = user.id;
 
-    res.send("You are signed in!!!");
+    res.redirect('/admin/products');
   }
 );
 
